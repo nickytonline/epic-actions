31bd8a0dcfdff0d76fcb480ea12295fdbd85dabd
mtfoley <mtfoley.mae@gmail.com>
2021-10-09 15:43:15 -0400

    implement message template

--- tweet-first-merge/action.js
+++ tweet-first-merge/action.js
@@ -2,15 +2,20 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const { context } = require('@actions/github/lib/utils');
 const repoToken = core.getInput('repo-token');
+const messageTemplate = core.getInput('message-template');
 const octokit = github.getOctokit(repoToken);
 
 async function run(){
     try {
         const ctx = github.context;
         const author = ctx.payload.pull_request?.user?.login ?? '';
+        const url = ctx.payload.pull_request?.html_url ?? '';
         if(await isFirstPull(author)){
             const twitterHandle = await getTwitterHandle(author);
-            core.info(`Ready to Tweet @${twitterHandle}`);
+            if(twitterHandle !== null){
+                let message = messageTemplate.replace(/%handle%/gi,twitterHandle).replace(/%url%/gi,url);
+                core.info(`Ready to Tweet:\n${message}`);
+            }
         }
     } catch(error){
         if(error instanceof Error) core.setFailed(error.message);
