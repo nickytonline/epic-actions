3275779a341bc4e20117d6b1f96a3a985943619f
mtfoley <mtfoley.mae@gmail.com>
2021-10-09 14:58:55 -0400

    remove addLabel, function getTwitterHandle

--- tweet-first-merge/action.js
+++ tweet-first-merge/action.js
@@ -9,29 +9,25 @@ async function run(){
         const ctx = github.context;
         const author = ctx.payload.pull_request?.user?.login ?? '';
         if(await isFirstPull(author)){
-            await addLabel(ctx.payload.pull_request?.number,'first-pull-request');
-            core.info('Ready to Tweet!');
+            const twitterHandle = await getTwitterHandle(author);
+            core.info(`Ready to Tweet @${twitterHandle}`);
         }
     } catch(error){
         if(error instanceof Error) core.setFailed(error.message);
     }
 }
+async function getTwitterHandle(login){
+    const {data: {twitter_username}} = await octokit.rest.users.getByUsername({username:login});
+    return twitter_username;
+}
 async function isFirstPull(login){
     const {owner,repo} = context.repo;
     const query = `repo:${owner}/${repo} is:pull-request author:${login}`;
     core.info(query);
-    const {data: result} = await octokit.rest.search.issuesAndPullRequests({
+    const {data: {total_count}} = await octokit.rest.search.issuesAndPullRequests({
         q:query,
         per_page:5
     });
-    core.info(JSON.stringify(result,null,"\t"));
-    return result.total_count <= 1;
-}
-async function addLabel(number,name){
-    await octokit.rest.issues.addLabels({
-        ...context.repo,
-        issue_number: number,
-        labels: [ name ]
-    })
+    return total_count <= 1;
 }
 run();
